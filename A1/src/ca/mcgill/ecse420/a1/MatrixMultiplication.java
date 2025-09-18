c8ca3a9f83cbd1fc6ee1becb5335823e49cc8429
Rafi Latif <rafilatif03@gmail.com>
2025-09-17 22:40:04 -0400

    created two new performance logging methods and updated gitignore

--- A1/src/ca/mcgill/ecse420/a1/MatrixMultiplication.java
+++ A1/src/ca/mcgill/ecse420/a1/MatrixMultiplication.java
@@ -2,28 +2,29 @@ package ca.mcgill.ecse420.a1;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class MatrixMultiplication {
 	
-	private static final int NUMBER_THREADS = 10;
-	private static final int MATRIX_SIZE = 1000;
+	private static final int NUMBER_THREADS = 13;
+	private static final int MATRIX_SIZE = 2000;
 
         public static void main(String[] args) {
 		
 		// Generate two random matrices, same size
-		double[][] a = generateRandomMatrix(MATRIX_SIZE, MATRIX_SIZE);
+		// double[][] a = generateRandomMatrix(MATRIX_SIZE, MATRIX_SIZE);
 		// double[][] b = generateRandomMatrix(MATRIX_SIZE, MATRIX_SIZE);
 		// System.out.println("-------------------------Parallel-----------------------------");
 		// printMatrix(parallelMultiplyMatrix(a, b));	
 		// System.out.println("-------------------------Sequential----------------------------");
-		// printMatrix(sequentialMultiplyMatrix(a, b));
+		// printMatrix(sequentialMultiplyMatrix(a, b, NUMBER_THREADS));
 
+		// System.out.println("Sequential time: " + measurePerformance(MATRIX_SIZE)[0]);
+		// System.out.println("Parallel time: " + measurePerformance(MATRIX_SIZE)[1]);
 
-		double[][] c = generateRandomMatrix(MATRIX_SIZE, MATRIX_SIZE);
-		double[][] d = generateRandomMatrix(MATRIX_SIZE, MATRIX_SIZE);
-		String[] times = measureMatrixMultiplicationPerformance(c, d);
-		System.out.println("Sequential time: " + times[0] + " ms");
-		System.out.println("Parallel time: " + times[1] + " ms");
+		createMatrixMultiplicationCSV(new int[] {100, 200, 500, 1000, 2000});
+		//createThreadPerformanceCSV(Runtime.getRuntime().availableProcessors());
 	}
 	
 	/**
@@ -33,12 +34,12 @@ public class MatrixMultiplication {
 	 * @param b is the second matrix
 	 * @return the result of the multiplication
 	 * */
-	public static double[][] sequentialMultiplyMatrix(double[][] a, double[][] b) {
-		double c[][] = new double[MATRIX_SIZE][MATRIX_SIZE];
+	public static double[][] sequentialMultiplyMatrix(double[][] a, double[][] b, int size) {
+		double c[][] = new double[size][size];
 
-        for (int i = 0; i < MATRIX_SIZE; i++) {
-            for (int j = 0; j < MATRIX_SIZE; j++) {
-                for (int k = 0; k < MATRIX_SIZE; k++)
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                for (int k = 0; k < size; k++)
                     c[i][j] += a[i][k] * b[k][j];
             }
         }
@@ -52,15 +53,15 @@ public class MatrixMultiplication {
 	 * @param b is the second matrix
 	 * @return the result of the multiplication
 	 * */
-        public static double[][] parallelMultiplyMatrix(double[][] a, double[][] b) {
-			double[][] c = new double[MATRIX_SIZE][MATRIX_SIZE];
+        public static double[][] parallelMultiplyMatrix(double[][] a, double[][] b, int size, int threads) {
+			double[][] c = new double[size][size];
 
-			ExecutorService executor = Executors.newFixedThreadPool(NUMBER_THREADS);
+			ExecutorService executor = Executors.newFixedThreadPool(threads);
 
-			for (int i = 0; i < MATRIX_SIZE; i++) {
-            for (int j = 0; j < MATRIX_SIZE; j++) {
+			for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 // Each element calculation is a task
-                DotProduct task = new DotProduct(a, b, c, i, j);
+                DotProduct task = new DotProduct(a, b, c, i, j, size);
                 executor.execute(task);
             }
         }
@@ -98,20 +99,22 @@ public class MatrixMultiplication {
 		private double[][] c;
 		private int row;
 		private int col;
+		private int size;
 
-		public DotProduct(double[][] a, double[][] b, double[][] c, int row, int col) {
+		public DotProduct(double[][] a, double[][] b, double[][] c, int row, int col, int size) {
 			this.a = a;
 			this.b = b;
 			this.c = c;
 			this.row = row;
 			this.col = col;
+			this.size = size;
 		}
 
 		@Override
 		public void run() {
 			// Calculate the dot product for one element
 			double sum = 0;
-			for (int k = 0; k < MATRIX_SIZE; k++) {
+			for (int k = 0; k < size; k++) {
 				sum += a[row][k] * b[k][col];
 			}
 			c[row][col] = sum;
@@ -121,26 +124,63 @@ public class MatrixMultiplication {
 
 	static void printMatrix(double M[][])
     {
-        for (int i = 0; i < MATRIX_SIZE; i++) {
-            for (int j = 0; j < MATRIX_SIZE; j++)
+        for (int i = 0; i < M[0].length; i++) {
+            for (int j = 0; j < M[0].length; j++)
                 System.out.print(M[i][j] + " ");
 
             System.out.println();
         }
     }
 
-	static String[] measureMatrixMultiplicationPerformance(double[][] a, double[][] b) {
+	static String[] measurePerformance(int size) {
+		double[][] a = generateRandomMatrix(size, size);
+		double[][] b = generateRandomMatrix(size, size);
+
 		long startSeq = System.currentTimeMillis();
-		sequentialMultiplyMatrix(a, b);
+		sequentialMultiplyMatrix(a, b, size);
 		long endSeq = System.currentTimeMillis();
 
 		long startPar = System.currentTimeMillis();
-		parallelMultiplyMatrix(a, b);
+		parallelMultiplyMatrix(a, b, size, NUMBER_THREADS);
 		long endPar = System.currentTimeMillis();
 
 		String seqTime = String.valueOf(endSeq - startSeq);
 		String parTime = String.valueOf(endPar - startPar);
 
-		return new String[] { seqTime, parTime };
+		return new String[] {seqTime, parTime};
+	}
+
+	static void createMatrixMultiplicationCSV(int[] sizes) {
+		String csvFile = "matrix_multiplication_performance.csv";
+		try (FileWriter writer = new FileWriter(csvFile, false)) { // 'false' to overwrite file each run
+			writer.append("Matrix Size,Sequential Time(ms),Parallel Time(ms)\n");
+			writer.flush();
+			for (int size : sizes) {
+				String[] times = measurePerformance(size);
+				writer.append(size + "," + times[0] + "," + times[1] + "\n");
+				writer.flush(); 
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	static void createThreadPerformanceCSV(int threads) {
+		String csvFile = "thread_performance.csv";
+		try (FileWriter writer = new FileWriter(csvFile)) {
+			writer.append("Threads,Time(ms)\n");
+			writer.flush(); 
+			for (int i = 1; i < (threads+1); i++) {
+				double[][] a = generateRandomMatrix(MATRIX_SIZE, MATRIX_SIZE);
+				double[][] b = generateRandomMatrix(MATRIX_SIZE, MATRIX_SIZE);
+				long startPar = System.currentTimeMillis();
+				parallelMultiplyMatrix(a, b, MATRIX_SIZE, i);
+				long endPar = System.currentTimeMillis();
+				writer.append(i + "," + (endPar - startPar) + "\n");
+				writer.flush();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
