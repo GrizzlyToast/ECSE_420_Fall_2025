0e3c1ba80a04649e0ab08351b3928357d8e1b247
Rafi Latif <rafilatif03@gmail.com>
2025-09-19 17:10:51 -0400

    clarified Q3.2 explanation

--- A1/src/ca/mcgill/ecse420/a1/DiningPhilosophers.java
+++ A1/src/ca/mcgill/ecse420/a1/DiningPhilosophers.java
@@ -6,7 +6,7 @@ public class DiningPhilosophers {
 	private static final int N = 5;  // Number of philosophers
 	private static final Semaphore[] chopsticks = new Semaphore[N];
 	/* Q3.2 Fair semaphore to ensure FIFO order:
-	 * The FIFO Semaphore queue ensures that only N-1 philosophers are permitted to pick up chopsticks. If the queue is full, once a philosopher has eaten, they are dequeued and the awaiting philospher is allowed to join the queue. Since the FIFO queue enforces priority based on order, it ensures that all philosphers get a chance to eat. This solution avoids starvation while promoting fairness.
+	 * The semaphore queue ensures that only N-1 philosophers are permitted to pick up chopsticks. If the queue is full, once a philosopher has eaten, they are dequeued and the awaiting philospher is allowed to join the queue. Semphores enforce a fairness policy based on longest wait time, therefore the ordering of the queue is based on FIFO. The queue ensures that all philosphers get a chance to eat based on the order they joined the queue. This solution avoids starvation while promoting fairness.
 	 */
 	private static final Semaphore queue = new Semaphore(N - 1, true);
 
